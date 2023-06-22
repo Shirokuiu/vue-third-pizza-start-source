8068e9838078b09deef19133a63f49bbde75291a
pavel <shirokuiuna@mail.ru>
2023-06-22 11:38:43 +0300

    Отрефакторил ConstructorIngredients.vue

--- frontend/src/modules/constructor/components/ConstructorIngredients.vue
+++ frontend/src/modules/constructor/components/ConstructorIngredients.vue
@@ -3,16 +3,8 @@
     <layout-constructor-title content-css-mod="ingredients">
       <template #title>Выберите ингредиенты</template>
       <template #content>
-        <constructor-ingredients-sauce
-          v-model="copiedSelectedSauce"
-          :sauces="sauces"
-        />
-
-        <constructor-ingredients-filling
-          :ingredients="ingredients"
-          @inc="incIngredient"
-          @dec="decIngredient"
-        />
+        <slot name="sauce" />
+        <slot name="ingredients" />
       </template>
     </layout-constructor-title>
   </div>
@@ -22,42 +14,5 @@
 export default {};
 </script>
 <script setup>
-import { ref, watch } from "vue";
 import LayoutConstructorTitle from "@/layouts/LayoutConstructorTitle.vue";
-import ConstructorIngredientsSauce from "@/modules/constructor/components/ConstructorIngredientsSauce.vue";
-import ConstructorIngredientsFilling from "@/modules/constructor/components/ConstructorIngredientsFilling.vue";
-
-const props = defineProps({
-  selectedSauce: {
-    type: Object,
-    required: true,
-  },
-  sauces: {
-    type: Array,
-    default: () => [],
-  },
-  ingredients: {
-    type: Array,
-    default: () => [],
-  },
-});
-
-const copiedSelectedSauce = ref(props.selectedSauce);
-
-const emit = defineEmits([
-  "update:selectedSauce",
-  "incIngredient",
-  "decIngredient",
-]);
-watch(copiedSelectedSauce, (newVal) => {
-  emit("update:selectedSauce", newVal);
-});
-
-const incIngredient = (incEvt) => {
-  emit("incIngredient", incEvt);
-};
-
-const decIngredient = (decEvt) => {
-  emit("decIngredient", decEvt);
-};
 </script>
